b789efcaee69fe3e7b452430b407771d2875a0cf
Mariano Huitron <mariano.14th@gmail.com>
2020-06-23 22:56:12 -0600

    Iniciando el servidor

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+
+// Settings
+app.set('port', process.env.PORT || 4000);
+
+
+// Routes
+app.use('/user', require('./routes/userRoutes'));
+
+
+// Server Listening
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+})
